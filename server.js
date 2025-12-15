f225c67b64b20ec606279e296c45e16234f4211b
rakhi <rakhisingh876.com@gmail.com>
2025-12-15 22:55:51 +0530

    setup socket.io and http server

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require('./src/app');
+const { createServer } = require('http');  //conver ur express into http server,needed when using socket.io
+const { Server } = require('socket.io');  // import
 
-app.listen(3000,()=>{
+
+
+const httpServer = createServer(app);  // wrap ur express inside http server
+
+const io = new Server(httpServer);  // creates a socket.io server
+
+io.on('connection',(socket)=>{
+    console.log("User connected:", socket.id); // socket.id = unique id created by the socket.io
+
+})
+httpServer.listen(3000,()=>{       //Starts your server on port 3000
     console.log("server is running")
 })
--- src/app.js
+++ src/app.js
@@ -4,4 +4,8 @@ const express = require('express');
 const app=express();
 
 
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
 module.exports = app;
